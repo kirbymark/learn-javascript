b0c525ef62c69b85411387fa49c2aab282da4f0f
Mark Kirby <mark@kirbyware.com>
2022-12-27 14:22:37 -0500

    linting

--- chapter-4/main2.js
+++ chapter-4/main2.js
@@ -1,14 +1,14 @@
 // returns a random integer in the range [m, n] (inclusive)
 function rand(m, n) {
     return m + Math.floor((n - m + 1)*Math.random());
-   }
+}
 
-   // randomly returns a string representing one of the six
+// randomly returns a string representing one of the six
 // Crown and Anchor faces
 function randFace() {
     return ["crown", "anchor", "heart", "spade", "club", "diamond"]
-    [rand(0, 5)];
-   }
+        [rand(0, 5)];
+}
 
 
 let funds = 50; // starting conditions
@@ -34,12 +34,12 @@ while(funds > 0 && funds < 100) {
             let face = randFace();
             bets[face] = bets[face] + bet;
             remaining = remaining - bet;
-        } while(remaining > 0)
+        } while(remaining > 0);
     }
     
     funds = funds - totalBet;
-    console.log('\tbets: ' +
-        Object.keys(bets).map(face => `${face}: ${bets[face]}p`).join(', ') +
+    console.log("\tbets: " +
+        Object.keys(bets).map(face => `${face}: ${bets[face]}p`).join(", ") +
         ` (total: ${totalBet}p)`);
     
     // roll dice
@@ -47,7 +47,7 @@ while(funds > 0 && funds < 100) {
     for(let roll = 0; roll < 3; roll++) {
         hand.push(randFace());
     }
-    console.log(`\thand: ${hand.join(', ')}`);
+    console.log(`\thand: ${hand.join(", ")}`);
     
     // collect winnings
     let winnings = 0;
